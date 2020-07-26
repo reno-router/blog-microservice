b065221de128fed690228d0b62fe121a80e47bdc
James Wright <james@jamesswright.co.uk>
2020-07-26 18:11:00 +0100

    Adds rudimentary method and route param handling

--- service/routes.ts
+++ service/routes.ts
@@ -1,5 +1,30 @@
-import { createRouteMap, textResponse } from "../deps.ts";
+import {
+  createRouteMap,
+  textResponse,
+  AugmentedRequest,
+  Status,
+} from "../deps.ts";
+
+function posts({ method, routeParams }: AugmentedRequest) {
+  const [id] = routeParams;
+
+  if (method === "GET") {
+    return textResponse(`You requested to retrieve ${id || "all posts"}`);
+  }
+
+  if (method === "POST") {
+    return textResponse(`You requested to create a new post`);
+  }
+
+  if (method === "PUT") {
+    return textResponse(`You requested to edit the contents of ${id}`);
+  }
+
+  return textResponse(`Method ${method} not allowed for /posts`, {
+    status: Status.MethodNotAllowed,
+  });
+}
 
 export default createRouteMap([
-  ["/", () => textResponse("Hello world!")],
+  ["/posts/*", posts],
 ]);
