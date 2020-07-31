16fe593585e1bf1d8db0dcd644a7b40629b84d79
James Wright <james@jamesswright.co.uk>
2020-07-31 22:57:31 +0100

    WIP cursor-based loop for seed data

--- data/03-posts.sql
+++ data/03-posts.sql
@@ -1,55 +1,96 @@
 use blogs;
 
-select @james_id:=id
+select id
+into @james_id
 from author
 where display_name = 'James Wright';
 
-select @joe_id:=id
+select id
+into @joe_id
 from author
 where display_name = 'Joe Bloggs';
 
-select @javascript_id:=id
+select id
+into @javascript_id
 from tag
 where display_name = 'JavaScript';
 
-select @typescript_id:=id
+select id
+into @typescript_id
 from tag
 where display_name = 'TypeScript';
 
-select @deno_id:=id
+select id
+into @deno_id
 from tag
 where display_name = 'Deno';
 
-select @rust_id:=id
+select id
+into @rust_id
 from tag
 where display_name = 'Rust';
 
-select @go_id:=id
+select id
+into @go_id
 from tag
 where display_name = 'Go';
 
-select @csharp_id:=id
+select id
+into @csharp_id
 from tag
 where display_name = 'C#';
 
-create temporary table post_metadata (meta JSON);
+create temporary table if not exists post_data (
+  post JSON
+);
 
-insert into post_metadata
+insert into post_data
 values
-  (CONCAT(
-    '{"id": "',
-      UUID(),
-    '", "author_id": "',
-      @james_id,
-    '", "title": "Deno 1.2.1 released',
-    '", "tag_ids": "[',
-      '"', @javascript_id, '"',
-      '"', @typescript_id, '"',
-      '"', @deno_id, '"',
-      '"', @rust_id, '"',
-    ']"}')
-  )
+  (JSON_OBJECT(
+    'id', UUID_TO_BIN(UUID()),
+    'author_id', @james_id,
+    'title', 'Deno 1.2.1 released',
+    'contents', 'Blog content here!',
+    'tag_ids', JSON_ARRAY(
+      @javascript_id,
+      @typescript_id,
+      @deno_id,
+      @rust_id
+    )
+  ));
 
-insert into post
-values
-  (UUID_TO_BIN(UUID()), @james_id, 'Deno 1.2.1 released', 'Blog contents here');
+delimiter $$
+create procedure dowhile()
+begin
+  declare finished int default 0;
+  declare pst JSON;
+
+  declare post_cursor cursor for
+  select post from post_data;
+
+  declare continue handler for
+  not found set finished = 1;
+
+  open post_cursor;
+
+  start transaction;
+
+  do_insert: loop
+  	fetch post_cursor into pst;
+
+    if finished = 1 then
+   	  leave do_insert;
+   	end if;
+
+    insert into post
+    values
+      (JSON_EXTRACT(pst, '$.id', '$.author_id', '$.title', '$.contents'));
+  end loop;
+
+  close post_cursor;
+
+  commit;
+end
+$$
+
+delimiter ;
