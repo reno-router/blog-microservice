fcecdae81b1a7e1cb1274f57f2e4d76c5aaa97d9
James Wright <james@jamesswright.co.uk>
2020-08-13 22:29:23 +0100

    Adds foreign reference constraint from post.author_id to author.id

--- data/01-tables.sql
+++ data/01-tables.sql
@@ -1,16 +1,16 @@
 create schema if not exists blogs;
 set schema 'blogs';
 
-create table if not exists post (
-  id uuid primary key not null,
-  author_id uuid not null,
-  title text not null,
-  contents text not null
-);
-
 create table if not exists author (
   id uuid primary key not null,
   display_name text not null
+);
+
+create table if not exists post (
+  id uuid primary key not null,
+  author_id uuid not null references author(id),
+  title text not null,
+  contents text not null
 );
 
 create table if not exists tag (
