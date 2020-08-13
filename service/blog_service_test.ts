891d7966a1e33e5e48d18d4ce253aabfeb320e4d
James Wright <james@jamesswright.co.uk>
2020-08-13 18:36:26 +0100

    Adds tests for blogService.edit()

--- service/blog_service_test.ts
+++ service/blog_service_test.ts
@@ -28,3 +28,35 @@ test("blogService.getPosts() should return all posts in the database", async () 
   assertEquals(actualPosts, posts);
   assertStrictEquals(dbService.query.callCount, 1);
 });
+
+test("blogService.getPost() should return the post for the given ID", async () => {
+  const posts = [{ id: "post 1" }];
+  const dbService = createDbService(posts);
+  const blogService = createBlogService(dbService);
+  const actualPost = await blogService.getPost("post 1");
+
+  assertEquals(actualPost, posts[0]);
+  assertStrictEquals(dbService.query.callCount, 1);
+});
+
+// TODO: tests for createPost
+
+test("blogService.editPost() should update the contents of the given post ID and return the row count", async () => {
+  const posts = [{ id: "post 1" }];
+  const dbService = createDbService(posts);
+  const blogService = createBlogService(dbService);
+  const rowCount = await blogService.editPost("post 1", "Updated contents!");
+
+  assertEquals(rowCount, 1);
+  assertStrictEquals(dbService.query.callCount, 1);
+});
+
+test("blogService.editPost() should return 0 if the underlying row count is undefined", async () => {
+  const posts = [{ id: "post 1" }];
+  const dbService = createDbService(posts, false);
+  const blogService = createBlogService(dbService);
+  const rowCount = await blogService.editPost("post 1", "Updated contents!");
+
+  assertEquals(rowCount, 0);
+  assertStrictEquals(dbService.query.callCount, 1);
+});
